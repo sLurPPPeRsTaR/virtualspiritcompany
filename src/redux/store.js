51617437ba345cbaad342bd1f9b6e020c1982aef
sLurPPPeRsTaR <monsherr@hotmail.com>
2022-04-30 15:30:20 +0700

    Default

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,21 +7,18 @@ const intialState = {
 const reducer = (state = intialState, action) => {
   if (action.type === '_INCREASE_COUNTER') {
     return {
-      ...state,
       counter: state.counter + 1,
     };
   }
   if (action.type === '_DECREASE_COUNTER') {
     if (state.counter > 0) {
       return {
-        ...state,
         counter: state.counter - 1,
       };
     }
   }
   if (action.type === '_RESET_COUNTER') {
     return {
-      ...state,
       counter: 0,
     };
   }
